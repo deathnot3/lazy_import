50688b5b483f23fb0203a4fee5f1f49048483099
deathnot3 <spyxpyro@hotmail.com>
2019-05-19 05:52:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from importlib import import_module
 
-def da_paja_importar(module_name):
+def lazy_import(module_name):
+	"""Funcion para importar un modulo cuando sea necesario usarlo"""
 	try:
+		# Importamos el modulo con import_module y se lo asignamos a module
 		module = import_module(module_name)
 
 	except ImportError:
 		print("Che ese modulo no existe")
 
 	else:
+		# Con esta linea de codigo le agregamos una clave mas al diccionario de globals y le asignamos como valor
+		# la variable con el modulo que acabamos de importar con import_module()
+		# De esta manera en lugar de escribir, por ejemplo, module.sqrt(4) al hacer lazy_import('math')
+		# pasariamos a escribir math.sqrt(4)
 		globals()[module_name] = module
 
-da_paja_importar('math')
+lazy_import('math')
 
 print(math.sqrt(4))
